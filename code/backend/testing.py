df337f81dd72f852ed8a17f1d08d9a09b3c24b7c
Shahroz Khan <shahrozkha83@gmail.com>
2025-07-11 16:13:48 +0500

    removed .env file

--- code/backend/testing.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt') # Download data for tokenization
-
-text = "This is a sample sentence, used for demonstration."
-tokens = word_tokenize(text, language='english', preserve_line=True)
-
-print(tokens)
